f8b9d7eea35f09eaf5d7f6df20835cb2f72a97fb
philwoz <61164019+philwoz@users.noreply.github.com>
2021-03-29 16:48:53 +0100

    added post and get

--- src/server.js
+++ src/server.js
@@ -1,11 +1,38 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
 
 
+app.get("/", (req, res)=> {
+    console.log(req.query);
+    res.send(`hello your age is ${req.query.age}` );
+    
+});
 
+app.get("/person/", (req, res)=> {
+
+    console.log(req.params.id)
+    res.send({
+        name: "phil",
+        age: "36",
+        id: req.params,
+    });
+
+    
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body)
+    res.send({message: "success"})
+})
+
+app.post("/person", (req, res) => {
+    console.log(req.body)
+    res.send({message: "success"})
+})
 
 
 app.listen(5000, ()=> {
-    console.log("listening on prot 5000")
+    console.log("listening on port 5000");
 });
